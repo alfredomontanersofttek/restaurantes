1d6e2638bce6ef65d8e1261807a223721a439578
SOFTTEKPROJECTS\luis.torpoco <luis.torpoco@softtek.com>
2026-04-15 16:02:59 +0200

    borrar crear y actualizar

--- restaurant/src/main/java/com/helloworld/restaurant/daos/plato/PlatoDao.java
+++ restaurant/src/main/java/com/helloworld/restaurant/daos/plato/PlatoDao.java
@@ -6,9 +6,19 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PlatoDao {
+<<<<<<< HEAD
     List<Plato> getPlatos();
 
     Optional<Plato> getPlatosById(int id);
 
     List<Plato> getPlatosByCalories(int calories);
+=======
+	List<Plato> getPlatos();
+	Optional<Plato> getPlatosById(int id);
+	List<Plato> getPlatosByCalories(int calories);
+
+	void deletePlato(int id);
+ 	void createPlato(Plato plato);
+	void updatePlato(Plato plato);
+>>>>>>> fa8847f (Metodo delete , create y update de plato)
 }
